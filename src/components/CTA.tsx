f3dbacf84734b4f4b194ff935ddb214d919ba71d
milan090 <milanmuhammed1@gmail.com>
2022-05-27 11:12:55 +0530

    feat: add zustand

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,41 +1,44 @@
 import { Link as ChakraLink, Button } from "@chakra-ui/react";
+import { useCounter } from "store/counter";
 
 import { Container } from "./Container";
 
-export const CTA = () => (
-  <Container
-    flexDirection="row"
-    position="fixed"
-    bottom={0}
-    width="full"
-    maxWidth="3xl"
-    py={3}
-  >
-    <Button
-      as={ChakraLink}
-      isExternal
-      href="https://chakra-ui.com"
-      variant="outline"
-      colorScheme="green"
-      rounded="button"
-      flexGrow={1}
-      mx={2}
+export const CTA = () => {
+  const increment = useCounter(state => state.increment);
+
+  return (
+    <Container
+      flexDirection="row"
+      position="fixed"
+      bottom={0}
       width="full"
+      maxWidth="3xl"
+      py={3}
     >
-      chakra-ui
-    </Button>
-    <Button
-      as={ChakraLink}
-      isExternal
-      href="https://github.com/vercel/next.js/blob/canary/examples/with-chakra-ui-typescript"
-      variant="solid"
-      colorScheme="green"
-      rounded="button"
-      flexGrow={3}
-      mx={2}
-      width="full"
-    >
-      View Repo
-    </Button>
-  </Container>
-);
+      <Button
+        variant="outline"
+        colorScheme="green"
+        rounded="button"
+        flexGrow={1}
+        mx={2}
+        width="full"
+        onClick={increment}
+      >
+        Increment +
+      </Button>
+      <Button
+        as={ChakraLink}
+        isExternal
+        href="https://github.com/milan090/nextjs-chakra-eslint-prettier-husky-zustand-template"
+        variant="solid"
+        colorScheme="green"
+        rounded="button"
+        flexGrow={3}
+        mx={2}
+        width="full"
+      >
+        View Repo
+      </Button>
+    </Container>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,107 +16,119 @@ import { Main } from "../components/Main";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import { CTA } from "../components/CTA";
 import { Footer } from "../components/Footer";
+import { useCounter } from "store/counter";
 
-const Index = () => (
-  <Container height="100vh">
-    <Box mt={20}>
-      <Hero title="Nextjs chakra eslint prettier husky zustand template" />
-    </Box>
+const Index = () => {
+  const count = useCounter(state => state.count);
+  return (
+    <Container height="100vh">
+      <Box mt={20}>
+        <Hero title="Nextjs chakra eslint prettier husky zustand template" />
+      </Box>
 
-    <Main>
-      <Text color="text">
-        Modern Frontend Template with <Code>Next.js</Code> +{" "}
-        <Code>chakra-ui</Code>
-        <Code>TypeScript</Code>+<Code>Zustand State Management</Code>
-      </Text>
+      <Main>
+        <Text>
+          Count: <Code>{count}</Code>
+        </Text>
+        <Text color="text">
+          Modern Frontend Template with <Code>Next.js</Code> +{" "}
+          <Code>chakra-ui</Code>
+          <Code>TypeScript</Code>+<Code>Zustand State Management</Code>
+        </Text>
 
-      <List spacing={3} my={0} color="text">
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            ESLint <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Prettier <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Husky <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Lint Staged <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Zustand <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+        <List spacing={3} my={0} color="text">
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="green.500" />
+            <ChakraLink
+              isExternal
+              href="https://nextjs.org"
+              flexGrow={1}
+              mr={2}
+            >
+              Next.js <LinkIcon />
+            </ChakraLink>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="green.500" />
+            <ChakraLink
+              isExternal
+              href="https://chakra-ui.com"
+              flexGrow={1}
+              mr={2}
+            >
+              Chakra UI <LinkIcon />
+            </ChakraLink>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="green.500" />
+            <ChakraLink
+              isExternal
+              href="https://chakra-ui.com"
+              flexGrow={1}
+              mr={2}
+            >
+              ESLint <LinkIcon />
+            </ChakraLink>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="green.500" />
+            <ChakraLink
+              isExternal
+              href="https://chakra-ui.com"
+              flexGrow={1}
+              mr={2}
+            >
+              Prettier <LinkIcon />
+            </ChakraLink>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="green.500" />
+            <ChakraLink
+              isExternal
+              href="https://chakra-ui.com"
+              flexGrow={1}
+              mr={2}
+            >
+              Husky <LinkIcon />
+            </ChakraLink>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="green.500" />
+            <ChakraLink
+              isExternal
+              href="https://chakra-ui.com"
+              flexGrow={1}
+              mr={2}
+            >
+              Lint Staged <LinkIcon />
+            </ChakraLink>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="green.500" />
+            <ChakraLink
+              isExternal
+              href="https://chakra-ui.com"
+              flexGrow={1}
+              mr={2}
+            >
+              Zustand <LinkIcon />
+            </ChakraLink>
+          </ListItem>
+        </List>
+      </Main>
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>
-        Created By{" "}
+      <DarkModeSwitch />
+
+      <Text mt={10}>
+        Template By{" "}
         <Link color="text" href="http://milan090.me/" target="_blank">
-          Milan090
+          @Milan090
         </Link>
       </Text>
-    </Footer>
-    <CTA />
-  </Container>
-);
+
+      <CTA />
+    </Container>
+  );
+};
 
 export default Index;
